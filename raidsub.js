1f33a3e2b25b5e823be0b7e6062f49a88ac5cfb5
ThatFave <michaelhesemann@waifu.club>
2020-09-05 23:45:36 +0200

    cleanup

--- raidsub.js
+++
@@ -1,9 +0,0 @@
-
-var submit = document.getElementById("submit");
-submit.onclick = function(){
-	var e = document.getElementById("select");
-	var mode = e.options[e.selectedIndex].text;
-	var line = Date() + " " + mode
-  fs.writeFile('text.txt', line)
-
-}
